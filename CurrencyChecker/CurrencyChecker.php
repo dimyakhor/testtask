cf75751dfb772c0a74dc7f4336a09d61b9e8ba5b
Dmitriy <mydial2009@gmail.com>
2019-01-27 21:11:01 +0700

    Реализовано получение курса валют из Rbc, вычисления объединены в один класс

--- CurrencyChecker/CurrencyChecker.php
+++ CurrencyChecker/CurrencyChecker.php
@@ -2,13 +2,50 @@
 
 class CurrencyChecker
 {
-    function getTodayCurrency()
+
+    const CBR_URL = 'http://www.cbr.ru/scripts/XML_daily.asp';
+    const RBC_URL = 'https://cash.rbc.ru/cash/json/converter_currency_rate/'; //?currency_from=USD&currency_to=RUR&source=cbrf&sum=1&date=2019-01-27';
+
+    const USD = 'USD';
+    const EUR = 'EUR';
+
+    const CBR_NUM_CODES = [
+        'USD' => 840,
+        'EUR' => 978
+    ];
+
+    private function getCurrencyFromCbr($currencyType, $timestamp)
     {
-        return CbrCurrencyGetter::getCurrency(date('d/m/Y'));
+        $url = self::CBR_URL . '?date_req=' . date('d/m/Y', $timestamp);
+        $xmlDataValues = simplexml_load_file($url);
+        foreach ($xmlDataValues->Valute as $valute) {
+            if ((int)$valute->NumCode === self::CBR_NUM_CODES[$currencyType]){
+                return [$currencyType => (float)str_replace(',','.',$valute->Value)];
+            }
+        }
+        return null;
     }
 
-//    function getTodayCurrency()
-//    {
-//        return RbcCurrencyGetter::getCurrency(date('d/m/Y'));
-//    }
+    private function generateRbcUrl($currencyType, $date) {
+        return self::RBC_URL . '?currency_from=' . $currencyType . '&currency_to=RUR&source=cbrf&sum=1&date=' . $date;
+    }
+
+    private function getCurrencyFromRbc($currencyType, $timestamp)
+    {
+        $date = date('Y-m-d',$timestamp);
+        return [
+            $currencyType => json_decode(file_get_contents($this->generateRbcUrl($currencyType, $date)))->data->sum_result
+        ];
+    }
+
+
+    function getTodayCurrency()
+    {
+        var_dump($this->getCurrencyFromRbc(self::USD, time()));
+        var_dump($this->getCurrencyFromCbr(self::USD, time()));
+        var_dump($this->getCurrencyFromRbc(self::EUR, time()));
+        var_dump($this->getCurrencyFromCbr(self::EUR, time()));
+
+       // return ;
+    }
 }
--- index.php
+++ index.php
@@ -4,10 +4,8 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-require_once 'CurrencyChecker/RbcCurrencyGetter.php';
-require_once 'CurrencyChecker/CbrCurrencyGetter.php';
 require_once 'CurrencyChecker/CurrencyChecker.php';
 
 $currencyChecker = new CurrencyChecker();
 
-var_dump($currencyChecker->getTodayCurrency());
+$currencyChecker->getTodayCurrency();
